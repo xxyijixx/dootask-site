096cec8cc513cf6274f21ae1dff994e4746f669c
nuohh <930699428@qq.com>
2025-01-14 14:44:50 +0800

    tip：更新日志优化

--- components/Downloadlog.vue
+++ components/Downloadlog.vue
@@ -26,7 +26,7 @@
           <img
             class="update-r-svg"
             id="dow_pic1"
-            src="/img/light/zh_dow_pic1.png"
+            :src="currentPicSrc"
             alt="DooTask,下载中心"
           />
         </div>
@@ -40,10 +40,10 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import { navigateTo } from '#app';
 
+const nuxtApp = useNuxtApp();
+
 const releases = ref([]);
 
-// 主题状态
-const isDarkMode = ref(false);
 
 const getLocalStorageItem = (key) => {
   const record = JSON.parse(localStorage.getItem(key));
@@ -90,17 +90,10 @@ const fetchReleases = async () => {
 
 // 切换主题，获取图片路径
 const currentPicSrc = computed(() => {
-  return isDarkMode.value
-    ? '/img/dark/zh_dow_pic1.png'
-    : '/img/light/zh_dow_pic1.png';
+  const theme = nuxtApp.$getTheme();
+  return `/img/${theme}/zh_dow_pic1.png`
 });
 
-// 监听主题变化
-const updateTheme = () => {
-  const htmlElement = document.documentElement;
-  const darkClass = htmlElement.classList.contains('dark');
-  isDarkMode.value = darkClass;
-};
 
 // 初始化并监听主题变化
 onMounted(() => {
@@ -110,25 +103,6 @@ onMounted(() => {
     releases.value = cachedReleases;
   } else {
     fetchReleases();
-  }
-
-  // 初始化主题
-  updateTheme();
-
-  // 监听主题变化
-  const observer = new MutationObserver(updateTheme);
-  observer.observe(document.documentElement, {
-    attributes: true,
-    attributeFilter: ['class'],
-  });
-
-  // 存储观察器以便后续卸载
-  window.themeObserver = observer;
-});
-
-onUnmounted(() => {
-  if (window.themeObserver) {
-    window.themeObserver.disconnect();
   }
 });
 </script>
